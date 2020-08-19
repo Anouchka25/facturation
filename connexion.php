c13a207eadb009930ed9cf64e311cc68f2165846
Anouchka25 <informaticaux@gmail.com>
2020-08-19 12:30:45 +0200

    modifs

--- connexion.php
+++ connexion.php
@@ -10,25 +10,47 @@
 
     <form action="<?= $_SERVER['PHP_SELF'] ?> " method="post">
         <input type="text" name="pseudo" placeholder="Votre pseudo">
-        <input type="text" name="password" placeholder="Votre mot de passe">
+        <input type="password" name="password" placeholder="Votre mot de passe">
         <input type="submit" value="Connexion">
     </form>
+
+
     <?php
-    $ps = "lucie25";
-    $pass = "toto";
-    $id_membre = 1;
+    //Inclusion des paramètres de connexion
+include_once('myparam.inc.php');
+
+//Connexion au serveur de base de données MySQL
+$idcom = new mysqli(MYHOST, MYUSER, MYPASS, "facturation");
+
+//Test de la connexion
+if(!$idcom){
+    echo "Connexion impossible";
+    exit(); //On arrete tout, on sort du script
+}
     
     if(!empty($_POST['pseudo']) && !empty($_POST['password'])){
-        if($_POST['pseudo'] == $ps && $_POST['password'] == $pass){
-        //on devrait faire un select et vérifier que les données saisies correspondent 
-        // à celles qui sont dans la base de données 
-        session_start(); //on créé une session
-        $_SESSION['pseudo'] = $ps;
-        $_SESSION['password'] = $pass;
-        $_SESSION['id_membre'] = $id_membre;
+       
+        $pseudo = $idcom->escape_string($_POST['pseudo']);
+        $password = $idcom->escape_string($_POST['password']);
 
-        header('Location: create-facture.php'); //Une fois connecté, on redirige l'utilisateur vers son espace client
-    }
+        //Ecrire la requete qui va récupérer le pseudo, password et id_membre
+        $requete = "SELECT password, id_membre FROM membres WHERE pseudo = '$pseudo' ";
+
+        $result = $idcom->query($requete);
+        
+        //$result->fetch_row() c'est-à-dire qu'on applique la fonction fetch_row à $result
+        $coord = $result->fetch_row();
+        // var_dump($coord);
+        if($coord && password_verify($_POST['password'], $coord[0])){
+            session_start();
+            $_SESSION['id_membre'] = $coord[1];
+            $_SESSION['pseudo'] = $_POST['password'];
+            
+            header('Location: create-facture.php');
+        }
+    
+    $idcom->close();
+       
     }
     else{ echo "Veuillez vous connecter"; }
        
